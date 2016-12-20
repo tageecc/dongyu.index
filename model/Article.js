ed612c61510178024628c20599806be49dce16fb
tagee <896932646@qq.com>
2016-12-21 00:47:48 +0800

    init

--- model/Article.js
+++ model/Article.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 var ArticleSchema = new mongoose.Schema({
     title: String,
     content: String,
-    type: Number,// 1 新闻动态 ,2 东娱作品 ,3 banner, 4 图片轮播, 5 二级栏目, 6 sub_article
+    type: Number,// 1 新闻动态 ,2 东娱作品 ,3 banner, 4 图片轮播, 5 二级栏目, 6 sub_article, 7 文章列表
     sub_article:[{type: Schema.Types.ObjectId, ref: 'Article'}],
     headimg: String,// 图片
     video: String,// 视频标签
